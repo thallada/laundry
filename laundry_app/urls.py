8effdbb1e6588c1ba7ab0f8aafeb4be9be7aeade
Tyler Hallada <tyler@hallada.net>
2014-07-28 18:52:58 -0400

    Fix urls.py for django >= 1.6

--- laundry_app/urls.py
+++ laundry_app/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url, include
 
 urlpatterns = patterns('laundry_app.views',
     url(r'^ajax/current/(?P<hall>\d+)/$', 'ajax_get_current', name='get_current_chart'),
